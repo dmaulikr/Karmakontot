6a0425e32d0c30f0f04f894c63106927f90d1727
Farshid Besharati <farshid@besharati.se>
2014-11-23 09:34:55 +0100

    First commit. Project creation in Xcode.

---
+++ Karmakontot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Karmakontot
+//
+//  Created by Farshid Besharati on 2014-11-23.
+//  Copyright (c) 2014 AllaUtomNiclas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'Karmakontot' do
+    pod 'MagicalRecord', '~> 2.2'
+    pod 'RestKit', '~> 0.24'
+end
+
+target 'KarmakontotTests' do
+
+end
+
