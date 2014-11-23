a7206cba5267c724960bdb0d9292657fef26d0b7
Viktor Gerdin <viktor.gerdin@klarna.com>
2014-11-23 10:07:17 +0100

    Server says Hello World

---
+++ server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello Kitty!"
+
+@app.route("/status")
+def karma():
+    return "Your Karma is gooooood!"
+
+if __name__ == "__main__":
+    app.run()
